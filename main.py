a798dc5c170de6550aca894301f58c4357e1d0bc
garizs <payka04@hotmail.com>
2020-07-08 04:42:27 +0400

    Core main.py

--- main.py
+++ main.py
@@ -8,4 +8,15 @@ bot = telebot.TeleBot(token)
 def start(message):
     bot.send_message(message.chat.id, 'Бот запущен')
 
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, 'Доступные команды:')
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_msg(message):
+    bot.send_message(message.chat.id, 'Не понимаю о чем вы')
+
+
 bot.polling()
